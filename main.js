5ff73daa62ed1cd169d44a43f0a44c78382d5bda
Your Name <you@example.com>
2023-10-09 20:15:28 +0500

    second

--- main.js
+++ main.js
@@ -9,31 +9,31 @@ if (a % 2 == 0) {
 
 
 
-let b = parseInt(Math.random() * 10000000)
-console.log(b);
-if (b % 5 == 0) {
-    console.log('b soni 5ga bolinadi');
+let e = parseInt(Math.random() * 10000000)
+console.log(e);
+if (e % 5 == 0) {
+    console.log('e soni 5ga bolinadi');
 } else {
-    console.log('b soni 5ga bolinmaydi');
+    console.log('e soni 5ga bolinmaydi');
 }
-if (b % 3 == 0) {
-    console.log('b soni 3ga bolinadi');
+if (e % 3 == 0) {
+    console.log('e soni 3ga bolinadi');
 } else {
-    console.log('b soni 3ga bolinmaydi');
+    console.log('e soni 3ga bolinmaydi');
 }
-if (b % 3 == 0) {
-    if (b % 5 == 0) {
+if (e % 3 == 0) {
+    if (e % 5 == 0) {
     } else {
-        console.log('b soni 3ga bolinadi 5ga bolinmaydi');
+        console.log('e soni 3ga bolinadi 5ga bolinmaydi');
     }
 } else {
-    console.log('b soni 3ga va 5ga bolinmaydi');
+    console.log('e soni 3ga va 5ga bolinmaydi');
 }
 if (b % 5 == 0) {
     if (b % 3 == 0) {
-        console.log('b soni 3ga va 5ga bolinadi');
+        console.log('e soni 3ga va 5ga bolinadi');
     } else {
-        console.log('b soni 5ga bolinadi 3ga bolinmaydi');
+        console.log('e soni 5ga bolinadi 3ga bolinmaydi');
     }
 }
 
@@ -41,36 +41,36 @@ if (b % 5 == 0) {
 
 
 
-let e = parseInt(Math.random() * 20)
+let b = parseInt(Math.random() * 20)
 let c = parseInt(Math.random() * 20)
-let difference1 = e-5
+let difference1 = b-5
 let difference2 = c-5
 difference1 = Math.abs(difference1)
 difference2 = Math.abs(difference2)
-console.log("e= " + e + " c= " + c)
-if (e > 5) {
-  if (e > 5) {
-    if (e > c) {
+console.log("b= " + b + " c= " + c)
+if (b > 5) {
+  if (c > 5) {
+    if (b > c) {
       console.log(c + " soni 5ga yaqinroq")
     } else {
-      console.log(e + " soni 5ga yaqinroq")
+      console.log(b + " soni 5ga yaqinroq")
     }
   } else if(difference1 > difference2) {
     console.log(c + " soni 5ga yaqinroq")
   
 }else{
-  console.log(e + " soni 5ga yaqinroq")
+  console.log(b + " soni 5ga yaqinroq")
 }
-} else if(e>5){
+} else if(c>5){
 if(difference1 > difference2) {
   console.log(c + " soni 5ga yaqinroq")
 }else if(difference1 < difference2){
-  console.log(e + " soni 5ga yaqinroq")
+  console.log(b + " soni 5ga yaqinroq")
 }else{
   console.log("ikkalasi ham teng yaqin")
 }
-}else if(e>c){
-  console.log(e + " soni 5ga yaqinroq")
+}else if(b>c){
+  console.log(b + " soni 5ga yaqinroq")
 }else{
   console.log(c + " soni 5ga yaqinroq")
 }
