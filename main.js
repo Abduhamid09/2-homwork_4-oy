04c2304016ee06bd7fe9b14bfbfa6bc75f0be514
Your Name <you@example.com>
2023-10-09 19:46:33 +0500

    first

---
+++ main.js
@@ -0,0 +1,50 @@
+let a = parseInt(Math.random() * 90)
+if (a % 2 == 0) {
+    console.log('a juft son')
+} else {
+    console.log('a toq son');
+}
+
+
+
+
+
+let b = parseInt(Math.random() * 10000000)
+console.log(b);
+if (b % 5 == 0) {
+    console.log('b soni 5ga bolinadi');
+} else {
+    console.log('b soni 5ga bolinmaydi');
+}
+if (b % 3 == 0) {
+    console.log('b soni 3ga bolinadi');
+} else {
+    console.log('b soni 3ga bolinmaydi');
+}
+if (b % 3 == 0) {
+    if (b % 5 == 0) {
+    } else {
+        console.log('b soni 3ga bolinadi 5ga bolinmaydi');
+    }
+} else {
+    console.log('b soni 3ga va 5ga bolinmaydi');
+}
+if (b % 5 == 0) {
+    if (b % 3 == 0) {
+        console.log('b soni 3ga va 5ga bolinadi');
+    } else {
+        console.log('b soni 5ga bolinadi 3ga bolinmaydi');
+    }
+}
+
+
+
+
+
+let c = parseInt(Math.random() * 90,)
+let d = parseInt(Math.random() * 90,)
+let f = Math.abs(c)
+let e = Math.abs(d)
+console.log('f soni 5 soniga yaqinroq'); 
+
+
